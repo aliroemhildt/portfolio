8b7a2525bc940a4ff708791736d034c6aed07ed6
Ali Roemhildt <aliroemhildt@gmail.com>
2024-03-05 20:59:06 -0700

    Setup components folder

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Work</li>
+        <li>About</li>
+        <li>Resume</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
