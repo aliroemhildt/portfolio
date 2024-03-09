6bbdb2c362111b3f7c87692f1baf3c3aeda9c4b4
Ali Roemhildt <aliroemhildt@gmail.com>
2024-03-09 13:23:46 -0700

    Move shared page classes to Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,9 +4,9 @@ import styles from './Layout.module.scss';
 
 export default function Layout({ children }) {
   return(
-    <div className={styles.layout}>
+    <div className={styles['layout-container']}>
       <Navbar />
-      <main>
+      <main className={styles['main-container']}>
           {children}
       </main>
     </div>
