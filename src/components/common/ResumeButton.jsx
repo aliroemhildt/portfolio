1677651fbfe2db42c0f24bcd4d101305aa8bb65d
Ali Roemhildt <aliroemhildt@gmail.com>
2024-03-10 14:03:09 -0600

    Add NavLink to resume button to open PDF in new tab

--- src/components/common/ResumeButton.jsx
+++ src/components/common/ResumeButton.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './ResumeButton.module.scss';
 
 export default function ResumeButton() {
-  // function to open file in new tab
-
   return (
-    <button className={styles['resume-button']}>
-      My Resume
-    </button>
+    <NavLink to='/resume.pdf' target='_blank'>
+      <button className={styles['resume-button']}>My Resume</button>
+    </NavLink>
   )
 }
---
+++ src/components/pages/ResumePDF.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import pdf from '../../assets/Resume.pdf';
+import { fixPdfPageScroll, clearPdfPageScroll } from '../../utils';
+import styles from './ResumePDF.module.scss';
+
+export default function ResumePDF() {
+  useEffect(() => {
+    // add no-scroll class to body element when component mounts
+    fixPdfPageScroll();
+
+    return () => {
+      // remove no-scroll class from body element when component unmounts
+      clearPdfPageScroll();
+    }
+  }, []);
+
+  return (
+    <iframe className={styles.frame} src={pdf}></iframe>
+  )
+}
