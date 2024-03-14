088c25466722067091146b42f792cb761cbf15ed
Ali Roemhildt <aliroemhildt@gmail.com>
2024-03-13 20:19:57 -0600

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Root from './components/layout/Root';
 import Work from './components/pages/Work';
-import Project from './components/pages/Project';
+import ChartToppersProject from './components/projects/ChartToppersProject';
+import LynnsListProject from './components/projects/LynnsListProject';
+import OverlookHotelProject from './components/projects/OverlookHotelProject';
+import RancidTomatillosProject from './components/projects/RancidTomatillosProject';
 import About from './components/pages/About';
 import Resume from './components/pages/Resume';
 import Contact from './components/pages/Contact';
@@ -12,7 +15,11 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={ <Root /> }>
       <Route index element={ <Work/> } />
-      <Route path='projects/:id' element={ <Project /> }/>
+      {/* <Route path='work/portfolio' element={<PortfolioProject /> } /> */}
+      <Route path='work/charttoppers' element={ <ChartToppersProject /> } />
+      <Route path='work/lynnslist' element={<LynnsListProject />} />
+      <Route path='work/rancidtomatillos' element={<RancidTomatillosProject /> } />
+      <Route path='work/overlookhotel' element={<OverlookHotelProject />} />
       <Route path='about' element={ <About /> } />
       <Route path='resume' element={ <Resume /> }/>
       <Route path='contact' element={ <Contact /> }/>
