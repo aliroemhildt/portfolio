726041c7fab6d83944b2b22cc618c0a587736f45
Ali Roemhildt <aliroemhildt@gmail.com>
2024-03-14 03:52:48 -0600

    Clean up component styling

--- src/components/common/ProjectCard.jsx
+++ src/components/common/ProjectCard.jsx
@@ -4,7 +4,7 @@ import styles from './ProjectCard.module.scss';
 
 export default function ProjectCard({ id, photo, title, description}) {
   return (
-    <NavLink to={`work/${id}`}>
+    <NavLink to={`./${id}`}>
       <div className={styles['card-container']}>
         <img className={styles['project-img']} src={photo} />
         <h2 className={styles.title}>{title}</h2>
