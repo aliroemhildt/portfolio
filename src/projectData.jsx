09ea09a4d209722ac2a135c5cff5921c441e3233
Ali Roemhildt <aliroemhildt@gmail.com>
2024-03-13 22:41:58 -0600

    Setup project data file to use later

---
+++ src/projectData.jsx
@@ -0,0 +1,14 @@
+const projectData = [
+  {
+    title: 'Chart Toppers'
+  },
+  {
+    title: `Lynn's List`
+  },
+  {
+    title: 'Rancid Tomatillos'
+  },
+  {
+    title: 'Overlook Hotel'
+  }
+]
