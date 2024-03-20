08fe24ef08ace9dfcfd29af8f836065c1678f6fe
Ali Roemhildt <aliroemhildt@gmail.com>
2024-03-20 14:13:16 -0600

    Add responsive hamburger menu

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.scss';
 import logo from '../../assets/logo.svg';
+import logoWhite from '../../assets/logo-white.svg';
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles['nav-container']}>
       <div className={styles['logo-container']}>
         <NavLink to='/' reloadDocument>
-          <img className={styles.logo} src={logo} alt='cursive letter A logo' />
+          <img className={`${styles.logo} ${styles.green}`} src={logo} alt='cursive letter A logo' />
+        </NavLink>
+        <NavLink to='/' reloadDocument>
+          <img className={`${styles.logo} ${styles.white}`} src={logoWhite} alt='cursive letter A logo' />
         </NavLink>
       </div>
-      <ul className={styles['nav-list']}>
+      <div className={styles['hamburger-menu']} onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={`${styles['nav-list']} ${menuOpen ? `${styles.open}` : ''}`}>
         <li>
           <NavLink
             to='/'
